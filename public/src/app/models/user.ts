a2e84bf6e93a881dca65350cb626aff7aeb6312b
Cam <cam.marshall940@yahoo.com>
2018-07-01 01:18:48 -0500

    Updated

--- public/src/app/models/user.ts
+++ public/src/app/models/user.ts
@@ -1,7 +1,16 @@
 export class User {
     _id: any;
-    name: string;
+    firstName: string;
+    lastName: string;
     email: string;
+    phone: number;
+    address: string;
+    city: string;
+    state: string;
+    zip: any;
+    role: string;
+    surveys: any;
+    clients: any;
     password: string;
     password_confirmation: any;
     createdAt: any;
