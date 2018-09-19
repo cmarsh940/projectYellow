d6f4a3b7fb291087f6caea55892994a8c9a17240
Cam <cam.marshall940@yahoo.com>
2018-09-19 17:09:11 -0500

    fixed minor bug in navigation, fixed overviews and added more reports

--- public/src/app/layout/overview-nav/overview-nav.component.ts
+++ public/src/app/layout/overview-nav/overview-nav.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { AuthService } from '../../auth/auth.service';
+import { Router } from '@angular/router';
+import { MatSnackBar, MatSnackBarConfig, MatSnackBarVerticalPosition, MatSnackBarHorizontalPosition } from '@angular/material';
+import { Client } from '../../global/models/client';
 
 @Component({
   selector: 'overview-nav',
@@ -9,12 +13,35 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./overview-nav.component.css']
 })
 export class OverviewNavComponent {
+  currentClient: Client = null;
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
     
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private _authService: AuthService,
+    private _router: Router,
+    public snackBar: MatSnackBar
+    ) {}
   
+  logout(): void {
+    this._authService.logout((res) => {
+      this.openSnackBar();
+      this.currentClient = null;
+      this._router.navigateByUrl('/');
+    });
+  }
+  openSnackBar() {
+    const config = new MatSnackBarConfig();
+    config.verticalPosition = this.verticalPosition;
+    config.horizontalPosition = this.horizontalPosition;
+    config.duration = 2500;
+    config.panelClass = ['logout-snackbar']
+    this.snackBar.open("You are logged out", '', config);
+  }
   }
--- public/src/app/overview/client-report/client-report.component.ts
+++ public/src/app/overview/client-report/client-report.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../../client/client.service';
+import { Client } from '../../global/models/client';
 
 @Component({
   selector: 'app-client-report',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-report.component.css']
 })
 export class ClientReportComponent implements OnInit {
+  dataSource: Client[];
+  errorMessage;
 
-  constructor() { }
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['name', 'surveys', 'sub', 'created'];
+
+  constructor(
+    private _clientService: ClientService
+  ) { }
 
   ngOnInit() {
+    this.loadAll();
+  }
+
+  loadAll(): Promise<any> {
+    const tempList = [];
+    return this._clientService.getAll()
+      .toPromise()
+      .then((result) => {
+        this.errorMessage = null;
+        result.forEach(participent => {
+          tempList.push(participent);
+        });
+        this.dataSource = tempList;
+        console.log("*** CLIENTS RETURNED ***:", this.dataSource);
+      })
+      .catch((error) => {
+        if (error === 'Server error') {
+          this.errorMessage = 'Could not connect to REST server. Please check your configuration details';
+        } else if (error === '404 - Not Found') {
+          this.errorMessage = '404 - Could not find API route. Please check your available APIs.';
+        } else {
+          this.errorMessage = error;
+        }
+      });
   }
 
 }
